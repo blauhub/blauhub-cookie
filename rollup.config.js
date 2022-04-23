0def09fa0da19f734182e15f0cdf657dd55ac9c5
Raoul Baron <raoulbaron96@gmail.com>
2022-04-23 07:53:27 +0200

    Use rollup for prod bundle

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import external from "rollup-plugin-peer-deps-external";
+import scss from "rollup-plugin-scss";
+import dts from "rollup-plugin-dts";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        name: "bhcookie",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      scss({
+        output: "dist/bhcookie.min.css",
+        outputStyle: "compressed",
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    external: [/\.s?css$/],
+    plugins: [dts()],
+  },
+];
